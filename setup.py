c1793cbeab63941534dc320d5078a5e666353d1a
Kieran Everett <kieraneverett03@gmail.com>
2021-04-06 17:14:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="kiwi-utils",
+    packages=find_packages(include=["kiwi-utils"]),
+    version="1.0.0",
+    description="A Python library for a few common things that I use when programming.",
+    author="Kieran Everett",
+    license="GNU General Public License v3.0"
+)
